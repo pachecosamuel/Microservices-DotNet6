10ecf7f40105aa8e5bfd7f5ac028c5cc79729df3
pachecosamuel <pacheco.samuel@hotmail.com>
2022-12-15 15:47:33 -0300

    feat: first migrations applied succesful

---
+++ S6-Microservices-DotNet6/GeekShopping/GeekShopping.ProductAPI/Model/Context/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions)
+        : base(dbContextOptions) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(GetConnectionString());
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        }
+
+
+        public string GetConnectionString()
+        {
+            return "Host=localhost;Port=5432;Pooling=true;Database=Geek_Shopping_Product_API;User Id=postgres;Password=159357sa@";
+        }
+
+    }
+}
