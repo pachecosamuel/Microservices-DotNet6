ed402c6849fd7a5d10d0e17ade9bcd31f5189683
pachecosamuel <pacheco.samuel@hotmail.com>
2022-12-17 08:59:56 -0300

    feat: repositories, services and controller added

---
+++ S8-Microservices-.Net6-OrganizingFirstMicroServiceArchiteture/GeekShopping/GeekShopping.ProductAPI/Controllers/pRODUCTController.cs
@@ -0,0 +1,39 @@
+﻿using GeekShopping.ProductAPI.Model.Data.DTOs;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> FindById(Guid id)
+        {
+            var product = await _repository.FindById(id);
+
+            if (product == null)
+                return NotFound();
+            else
+                return Ok(product);
+        }
+
+    }
+}
