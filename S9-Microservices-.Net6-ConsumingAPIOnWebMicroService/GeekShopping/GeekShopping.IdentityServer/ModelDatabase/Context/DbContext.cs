fe2229e72ac0bc491a64ecc3ed83ecaf5f7b0aff
pachecosamuel <pacheco.samuel@hotmail.com>
2022-12-20 12:36:40 -0300

    feat: configuring resources, scopes and client on IS6

---
+++ S9-Microservices-.Net6-ConsumingAPIOnWebMicroService/GeekShopping/GeekShopping.IdentityServer/ModelDatabase/Context/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.ModelDatabase.Context
+{
+    public class DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbContext(DbContextOptions<DbContext> dbContextOptions)
+        : base(dbContextOptions) { }
+    }
+}
