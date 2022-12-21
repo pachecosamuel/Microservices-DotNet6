ed4fcf8bdaebf5f3bf55fec5866a528111b9af64
pachecosamuel <pacheco.samuel@hotmail.com>
2022-12-21 10:27:31 -0300

    feat: identity configuration almost done

--- S9-Microservices-.Net6-ConsumingAPIOnWebMicroService/GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ S9-Microservices-.Net6-ConsumingAPIOnWebMicroService/GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,33 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.ModelDatabase.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -10,6 +36,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
